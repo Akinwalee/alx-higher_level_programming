4edf7d246fdc1fa613e75bdddb28a399103b62dc
Akinwalee <obatulafuad@gmail.com>
2023-10-31 16:11:47 +0000

    Updated files

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,6 +4,3 @@ def uppercase(str):
     for i in str:
         up += '{}'.format(chr(ord(i) - 32) if ord(i) > 96 else i)
     print(up)
-
-uppercase("best")
-uppercase("Best School 98 Battery street")
