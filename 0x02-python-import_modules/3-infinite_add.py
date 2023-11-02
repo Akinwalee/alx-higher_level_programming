3ce07ee836b018db546f480ee166ea0dd73f21ca
Akinwalee <obatulafuad@gmail.com>
2023-11-02 23:25:55 +0000

    Updated files

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,8 +2,14 @@
 from sys import argv
 
 add_argv = 0
+argc = len(argv)
 
-for i in argv:
-    add_argv += int(i)
+for i in range(argc):
+    if i == 0:
+        continue
+    add_argv = add_argv + int(argv[i])
 
 print(add_argv)
+
+if __name__ == "__main__":
+    pass
