841f5f4c17fa11dde6b43c4b82a0cde500ac7e8c
Akinwalee <obatulafuad@gmail.com>
2023-11-02 22:28:01 +0000

    Updated files

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-import add_0
+from add_0 import add
 
 a = 1
 b = 2
-print("{} + {} = {}".format(a, b, add(a, b)))
+res = add(a, b)
+print("{} + {} = {}".format(a, b, res))
 
 if __name__ == '__main__':
-    main()
+    pass
