eb2b17a26386d19d055ec3f49e3c91f6b493ec0a
Akinwalee <obatulafuad@gmail.com>
2023-11-02 23:07:07 +0000

    Updated files

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from sys import argv
+
+argc = len(argv)
+
+if argc == 1:
+    print("0 arguments.")
+else:
+    print("{} argument{}:".format(argc - 1, "" if argc == 2 else "s"))
+    for i in range(0, argc):
+        if i == 0:
+            continue
+        print("{}: {}".format(i, argv[i]))
