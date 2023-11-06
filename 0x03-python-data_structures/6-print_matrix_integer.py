8df7fc516c990693a4aaf8fee44f715ed198ac8a
Akinwalee <obatulafuad@gmail.com>
2023-11-06 05:41:01 +0000

    Updated files

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    rang = len(array) - 1
     for array in matrix:
+        rang = len(array) - 1
         for x, i in enumerate(array):
             print("{:d}{}".format(i, " " if x < rang else '\n'), end="")
