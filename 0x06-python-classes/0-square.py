ec45d9d64717745e919e0865495ef7c338380237
Akinwalee <obatulafuad@gmail.com>
2023-11-22 06:17:07 +0000

    Updated files

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
 class Square:
+    """This is an empty class that defines square objects.
+    The first task in a project of 9 tasks on object oriented programming.
+    This class does nothing (for now). It will be iterated over throughout
+    this project to build a fully functional Squares class.
+    """
     pass
