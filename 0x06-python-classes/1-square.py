34a7447fd79ce91ed95dbe07d176ac33f99cb644
Akinwalee <obatulafuad@gmail.com>
2023-11-22 06:23:04 +0000

    Updated files

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""Defining a square class"""
+
+
+class Square:
+    """This is a square class with a size private attribute"""
+
+    def __init__(self, size):
+        self.__size = size
