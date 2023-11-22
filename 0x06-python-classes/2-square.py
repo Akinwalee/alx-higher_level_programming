c1b3987b299b1aacfcbbdf05a6a09cc494e8b414
Akinwalee <obatulafuad@gmail.com>
2023-11-22 06:27:30 +0000

    Updated files

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""Defining a square class"""
+
+
+class Square:
+    """This is a square class with a size private attribute
+    It implements a type check to enure that the ize attribute is a valid integer"""
+
+    def __init__(self, size=0):
+        try:
+            self.__size = size
+        except TypeError:
+            print("size must be an integer")
+        except ValueError:
+            print("size must be >= 0")
