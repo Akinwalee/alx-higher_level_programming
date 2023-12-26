754fd8fc75fbc8d862cdc324a0ed7263dc94a491
Akinwalee <obatulafuad@gmail.com>
2023-12-26 23:51:06 +0000

    Updated files

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import unittest
+
+"""Unittest for max_integer(list=[]) function"""
+
+max_integer = __import__('6-max_integer').max_integer
+
+class TetMaxInteger(unittest.TestCase):
+
+    def test_max_integer(self):
+        #Test cases
+        self.assertAlmostEqual(max_integer([1, 2, 3, 4]), 4)
+        self.assertAlmostEqual(max_integer([1, 3, 4, 2]), 4)
+        self.assertAlmostEqual(max_integer([0, -1, -4, -99, -5]), 0)
+        self.assertAlmostEqual(max_integer([9]), 9)
