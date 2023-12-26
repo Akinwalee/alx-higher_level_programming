0ad464cf480ae284296b671e41b6605fdd352694
Akinwalee <obatulafuad@gmail.com>
2023-12-26 15:29:39 +0000

    Updated files

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,5 +1,12 @@
-#!/ur/bin/python3
+#!/usr/bin/python3
+"""A module that adds two integers"""
+
+
 def add_integer(a, b=98):
+    """This function accepts two integers/floats
+    and returns their su
+    m"""
+
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
     if not isinstance(b, int) and not isinstance(b, float):
