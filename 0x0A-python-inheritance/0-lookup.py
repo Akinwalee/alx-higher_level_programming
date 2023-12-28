431f9e9b677ab85c8f1ae2d3dacc6bfe893860b3
Akinwalee <obatulafuad@gmail.com>
2023-12-28 10:39:03 +0000

    Updated files

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""A module that returns the list of available
+attributes and methods of an object
+"""
+
+
+def lookup(obj):
+    """Returns a list containing
+    all attributes of obj
+    """
+
+    return(obj.__dir__())
