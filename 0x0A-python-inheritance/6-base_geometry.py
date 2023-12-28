ab0d75421f8b0ba5f8e4f64ed64688fa079ede2e
Akinwalee <obatulafuad@gmail.com>
2023-12-28 14:16:55 +0000

    Updated files

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""A Geometry Modules for different \
+        gemetric shapes"""
+
+
+class BaseGeometry:
+    """Base class for different geometric shapes"""
+    def area(self):
+        raise Exception("area() is not implemented")
