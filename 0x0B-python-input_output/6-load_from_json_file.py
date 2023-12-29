96c5e2b48fe86e78066110f6350c43f5d55c2129
Akinwalee <obatulafuad@gmail.com>
2023-12-29 17:19:42 +0000

    Updated files

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -7,5 +7,9 @@ import json
 
 def load_from_json_file(filename):
     """Creates an object from filename"""
-    with open(filename, "r", encoding="utf-8") as f:
-        return (json.load(f))
+
+    try:
+        with open(filename, "r", encoding="utf-8") as f:
+            return (json.load(f))
+    except FileNotFoundError:
+        return []
