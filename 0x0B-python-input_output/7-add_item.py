d109bc371f2d4d64bd555bd8311fc4e600aadfea
Akinwalee <obatulafuad@gmail.com>
2023-12-29 17:07:14 +0000

    Updated files

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,7 +7,11 @@ import sys
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-my_list = load_from_json_file("add_item.json")
+
+try:
+    my_list = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    my_list = []
 
 for i in range(1, len(sys.argv)):
     my_list.append(sys.argv[i])
