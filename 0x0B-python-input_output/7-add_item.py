fe06defd0c0fd7e46517130195acc929a3cae7fa
Akinwalee <obatulafuad@gmail.com>
2023-12-29 16:40:11 +0000

    Updated files

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -10,9 +10,7 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 my_list = []
 
-for i in range(len(sys.argv)):
-    if i == 0:
-        continue
+for i in range(1, len(sys.argv)):
     my_list.append(sys.argv[i])
 
 save_to_json_file(my_list, "add_item.json")
