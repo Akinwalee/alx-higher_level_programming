94e5c8174743fb9a39d0181f66567168bd1f869c
Akinwalee <obatulafuad@gmail.com>
2023-12-29 16:12:04 +0000

    Updated files

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Add items to Python list and save as JSON"""
+
+
+import json
+import sys
+
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+my_list = []
+
+for i in range(sys.argv):
+    if i == 0:
+        continue
+    my_list.append(i)
+
+save_to_json_file(my_list, "add_item.json")
+load_from_json_file("add_item.json")
