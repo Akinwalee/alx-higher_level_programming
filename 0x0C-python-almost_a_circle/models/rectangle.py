db6f4d4ac7d8cd13bf9a01c7012705f61252d432
Akinwalee <obatulafuad@gmail.com>
2024-01-02 09:55:07 +0000

    Updated files

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -5,6 +5,7 @@
 class Rectangle(Base):
     """Creates a rectangle class from Base class"""
     def __init__(self, width, height, x=0, y=0, id=None):
+        """Constructor for Rectangle class"""
         self.__width = width
         self.heigth = height
         self.__x = x
@@ -12,32 +13,40 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """width getter"""
         return (self.__width)
 
     @width.setter
     def width(self, value):
+        """width setter"""
         self.__width = value
 
     @property
     def height(self):
+        """height getter"""
         return (self.__height)
 
     @height.setter
     def height(self, value):
+        """height setter"""
         self.__height = value
 
     @property
     def x(self):
+        """x getter"""
         return (self.__x)
 
     @x.setter
     def x(self, value):
+        """x setter"""
         self.__x = value
 
     @property
     def y(self):
+        """y getter"""
         return (self.__y)
 
     @y.setter
     def y(self, value):
+        """y setter"""
         self.__y = value
