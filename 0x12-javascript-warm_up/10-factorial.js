67625019946424dae31979afb867cc75ca506959
Akinwalee <obatulafuad@gmail.com>
2024-02-13 07:13:00 +0000

    Updated files

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 const n = Number(process.argv[2]);
 
-function factorial(n) {
+function factorial (n) {
   if (isNaN(n) || n === 1 || n === 0) {
     return (1);
   }
   return (n * factorial(n - 1));
-};
+}
 
 console.log(factorial(n));
