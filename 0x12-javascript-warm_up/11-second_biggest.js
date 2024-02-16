59acdc0de44ae8fbbe0662e2df879b99a2bbf300
Akinwalee <obatulafuad@gmail.com>
2024-02-16 06:58:22 +0000

    Updated files

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,12 +5,15 @@ const args = process.argv;
 if (args.length < 4) {
   console.log(0);
 } else {
-  let max = Number(args[2]);
-  let max2 = Number(args[2]);
+  let max = Number.NEGATIVE_INFINITY;
+  let max2 = Number.NEGATIVE_INFINITY;
   for (let i = 0; i < args.length; i++) {
-    if (args[i] > max) {
+    if (Number(args[i]) > max) {
       max2 = max;
-      max = args[i];
+      max = Number(args[i]);
+    }
+    if (Number(args[i]) > max2 && Number(args[i]) !== max) {
+      max2 = Number(args[i]);
     }
   }
   console.log(max2);
