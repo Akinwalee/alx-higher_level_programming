ead210b0bfefdc5600d450408b7ed4808ffec2c3
Akinwalee <obatulafuad@gmail.com>
2024-02-17 14:32:39 +0000

    Updated files

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -3,10 +3,10 @@
 exports.esrever = function (list) {
   const len = list.length;
 
-  for (let i = 0; i < len; i++) {
+  for (let i = 0; i < len / 2; i++) {
     const temp = list[i];
-    list[i] = list[len - i];
-    list[len - i] = temp;
+    list[i] = list[len - i - 1];
+    list[len - i - 1] = temp;
   }
   return (list);
 };
