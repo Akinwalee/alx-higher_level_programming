7c175a7ac33b2947bc06b08ce97b6047e6d66cf2
Akinwalee <obatulafuad@gmail.com>
2024-02-17 17:22:29 +0000

    Updated files

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -6,15 +6,9 @@ if (process.argv.length === 5) {
   const [file1, file2, file3] = process.argv;
 
   fs.readFile(file1, (err, content) => {
-    if (err) {
-      throw err;
-    }
-    fs.writeFile(file3, content);
+    fs.writeFile(file3, content.toString());
   });
   fs.readFile(file2, (err, content) => {
-    if (err) {
-      throw err;
-    }
-    fs.writeFile(file3, content);
+    fs.writeFile(file3, content.toString());
   });
 }
