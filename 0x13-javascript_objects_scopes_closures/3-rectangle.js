a8d205ab80e3cc0eb0659c3ee412ba52f2b0b83e
Akinwalee <obatulafuad@gmail.com>
2024-02-17 07:30:21 +0000

    Updated files

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -6,18 +6,19 @@ class Rectangle {
       this.width = w;
       this.height = h;
     }
-    this.print = function (w, h) {
-      let i = 0;
-      while (i < this.height) {
-        let j = 0;
-        while (j < this.width) {
-          process.stdout.write('X');
-          j++;
-        }
-        console.log('');
-        i++;
+  }
+
+  print () {
+    let i = 0;
+    while (i < this.height) {
+      let j = 0;
+      while (j < this.width) {
+        process.stdout.write('X');
+        j++;
       }
-    };
+      console.log('');
+      i++;
+    }
   }
 }
 
