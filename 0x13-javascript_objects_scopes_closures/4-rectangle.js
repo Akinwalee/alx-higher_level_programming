66f7609f077efdb50a53e52c5a0fe93721f3ed99
Akinwalee <obatulafuad@gmail.com>
2024-02-17 07:34:36 +0000

    Updated files

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,36 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let i = 0;
+    while (i < this.height) {
+      let j = 0;
+      while (j < this.width) {
+        process.stdout.write('X');
+        j++;
+      }
+      console.log('');
+      i++;
+    }
+  }
+
+  rotate () {
+    const temp = this.height;
+    this.height = this.width;
+    this.width = temp;
+  }
+
+  double () {
+    this.height *= 2;
+    this.width *= 2;
+  }
+}
+
+module.exports = Rectangle;
