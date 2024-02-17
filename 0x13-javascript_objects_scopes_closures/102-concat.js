21f8477579c1d296f36ba901f6276db66e88984b
Akinwalee <obatulafuad@gmail.com>
2024-02-17 17:46:15 +0000

    Updated files

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -5,12 +5,12 @@ const fs = require('fs');
 if (process.argv.length === 5) {
   const [file1, file2, file3] = process.argv;
 
-  fs.readFile(file1, (err, content) => {
+  fs.readFile(file1, (err, content1) => {
     if (err) throw err;
     fs.readFile(file2, (err, content2) => {
       if (err) throw err;
 
-      content = Buffer.concat([content, content2]);
+      const content = Buffer.concat([content1, content2]);
       fs.writeFile(file3, content, (err) => {
         if (err) throw err;
       });
