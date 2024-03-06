07b94bbd0947df7e5999e37c1626473e95f5ea7c
Akinwalee <obatulafuad@gmail.com>
2024-03-06 01:18:01 +0000

    Updated files

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 #Script that sends a request to a server and prints the response lenght in bytes
 
-if [ -z "$1" ]; then
-	exit 1
-fi
-
-response=$(curl -sI "$1")
-c_length=$(echo "$response" | grep -i 'content-length:' | awk '{print $2}' | tr -d '\r')
-
-echo "$c_length"
+curl -s "$1" | wc -c
