d63969430408da0271fe97c3e6fad3e51a932878
Akinwalee <obatulafuad@gmail.com>
2024-03-06 01:19:43 +0000

    Updated files

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 #Script that sends a request to a server and prints the response lenght in bytes
-
 curl -s "$1" | wc -c
--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # Print response for 200 status code
-
 curl -sL "$1"
